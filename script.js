944dcaa666a9b1a2523725bc7346ff8540f24b5c
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-08-11 08:29:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+ function calculateArea() {
+            // Get the radius from the user using the prompt() function
+            let radius = prompt("Enter the radius of the circle:");
+            // write you code here and display the result to the user
+           
+}
+calculateArea();
